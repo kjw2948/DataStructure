284128b0245f3b056da9ddbbc9d917c86f9d0513
kjw2948 <kjw970313@cau.ac.kr>
2024-01-25 19:48:20 +0900

    stack

--- src/solution/stack/Test.java
+++ src/solution/stack/Test.java
@@ -1,4 +1,20 @@
 package solution.stack;
 
 public class Test {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.pop());
+        stack.push(3);
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        stack.push(4);
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.pop());
+        System.out.println(stack.isEmpty());
+
+    }
 }
